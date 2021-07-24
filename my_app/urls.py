a8568765140d89a7816c0c91135dd3593dc6f736
Karen Thomas <68983668+thomasknashville@users.noreply.github.com>
2021-07-24 13:15:02 -0500

    add fe requirements file

--- my_app/urls.py
+++ my_app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    # path('new-search/', views.new_search, name="new_search"),
+    path('new-search/', views.new_search, name="new_search"),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -2,5 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def home(request):
     return render(request, 'base.html')
+
+
+def new_search(request):
+    search = request.POST.get('search')
+    print(search)
+    stuff_for_frontend = {
+        'search': search,
+    }
+    return render(request, 'my_app/new_search.html', )
